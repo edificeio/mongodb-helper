3d9a8c3e74cce0a630a8dab851b5cae68cb89c86
dboissin <damien.boissin@web-education.net>
2024-01-25 11:45:41 +0100

    feat: SRE-1807 upgrade mongodb driver

--- src/main/java/fr/wseduc/mongodb/MongoQueryBuilder.java
+++ src/main/java/fr/wseduc/mongodb/MongoQueryBuilder.java
@@ -18,14 +18,13 @@ package fr.wseduc.mongodb;
 
 import com.mongodb.DBObject;
 import com.mongodb.QueryBuilder;
-import com.mongodb.util.JSON;
 import io.vertx.core.json.JsonObject;
 
 public class MongoQueryBuilder {
 
 	public static JsonObject build(QueryBuilder queryBuilder) {
 		DBObject dbo = queryBuilder.get();
-		return new JsonObject(JSON.serialize(dbo));
+		return new JsonObject(dbo.toMap());
 	}
 
 }
